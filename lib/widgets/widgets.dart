6fc5b5af7494f3a299e320e8bfcc635dd341ac4e
philliu198188 <ffash2@163.com>
2026-06-01 18:40:47 +0800

    fix: replace missing shimmer_widgets.dart export with skeleton_loader.dart

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,7 +1,9 @@
 /// 易比价 公共 Widget 组件库
 library widgets;
 
-export 'shimmer_widgets.dart';
+export 'skeleton_loader.dart';
+export 'empty_state.dart';
+export 'error_retry.dart';
 export 'product_card.dart';
 export 'price_tag.dart';
 export 'search_bar.dart';
